790bc789bb08100ed7c3436e404643afb8a9e485
Ginga Kennis <gingakennis123@icloud.com>
2023-09-18 12:52:23 +0900

    fix robotiq gripper and confirm action

--- src/universal-robot/robotiq/robotiq_modbus_rtu/src/robotiq_modbus_rtu/comModbusRtu.py
+++ src/universal-robot/robotiq/robotiq_modbus_rtu/src/robotiq_modbus_rtu/comModbusRtu.py
@@ -84,14 +84,13 @@ class communication:
    def getStatus(self, numBytes):
       """Sends a request to read, wait for the response and returns the Gripper status. The method gets the number of bytes to read as an argument"""
       numRegs = int(ceil(numBytes/2.0))
-
       #To do!: Implement try/except
       #Get status from the device
-      response = self.client.read_holding_registers(0x07D0, numRegs, unit=0x0009)
-
+      response = self.client.read_holding_registers(0x07D0, numRegs, slave = 9,  unit=0x0009)
+      response2 = self.client.read_input_registers(address=0x07D0, count=numRegs,slave = 9 ,unit= 0x0009)
+      print("response is "+str(response2))
       #Instantiate output as an empty list
       output = []
-
       #Fill the output with the bytes in the appropriate order
       for i in range(0, numRegs):
          output.append((response.getRegister(i) & 0xFF00) >> 8)
